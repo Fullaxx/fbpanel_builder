56b258e40ead6061dfb3c806a729b843954449a7
Brett Kuskie <fullaxx@gmail.com>
2025-09-14 11:41:06 +0000

    Delete scripts/make_package.sh

--- scripts/compile_fullaxx_fbpanel_eleksir.sh
+++ scripts/compile_fullaxx_fbpanel_eleksir.sh
@@ -12,4 +12,7 @@ cmake \
 
 make -j
 
-/scripts/make_package.sh
+mkdir /tmp/fbpanel
+make install DESTDIR=/tmp/fbpanel
+gzip -9 /tmp/fbpanel/usr/share/man/man1/fbpanel.1
+( cd /tmp/fbpanel; tar cvf /tmp/fbpanel.tar * )
--- scripts/make_package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-mkdir /tmp/fbpanel
-make install DESTDIR=/tmp/fbpanel
-gzip -9 /tmp/fbpanel/usr/share/man/man1/fbpanel.1
-( cd /tmp/fbpanel; tar cvf /tmp/fbpanel.tar * )
