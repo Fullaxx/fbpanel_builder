5427e5ab7c66b6dbd60e694a9ed7e33ee1154aea
Brett Kuskie <fullaxx@gmail.com>
2025-09-14 11:17:10 +0000

    gzip the man page

--- scripts/make_package.sh
+++ scripts/make_package.sh
@@ -4,5 +4,5 @@ set -e
 
 mkdir /tmp/fbpanel
 make install DESTDIR=/tmp/fbpanel
-#tree /tmp/fbpanel
+gzip -9 /tmp/fbpanel/usr/share/man/man1/fbpanel.1
 ( cd /tmp/fbpanel; tar cvf /tmp/fbpanel.tar * )
