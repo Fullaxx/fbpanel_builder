198cd01f1334ea1571ff06332be804e28d4cffa2
Brett Kuskie <fullaxx@gmail.com>
2026-02-18 12:08:00 +0000

    Comment out old fbpanel tests

--- test_all.sh
+++ test_all.sh
@@ -8,7 +8,7 @@ for TAG in ${TAGS}; do
   echo "Testing ${TAG} ..."
   IMAGE="ghcr.io/fullaxx/fbpanel_builder:${TAG}"
 #  docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_eleksir_mainline.sh
-  docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_fullaxx_fbpanel_eleksir.sh
-  docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_fullaxx_fbpanel3_berte.sh
+#  docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_fullaxx_fbpanel_eleksir.sh
+#  docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_fullaxx_fbpanel3_berte.sh
   docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_fullaxx_fbpanel3.sh
 done
