8d7ee6896fb8349113ee27e3dbbd91c775914d98
Brett Kuskie <fullaxx@gmail.com>
2026-02-23 21:27:04 +0000

    Remove obsolete fullaxx_fbpanel3 project

--- 03_test_all.sh
+++ 03_test_all.sh
@@ -10,6 +10,5 @@ for TAG in `ls -1 Dockerfile.* | cut -d. -f2`; do
 #  docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_eleksir_mainline.sh
 #  docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_fullaxx_fbpanel_eleksir.sh
 #  docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_fullaxx_fbpanel3_berte.sh
-#  docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_fullaxx_fbpanel3.sh
   docker run -it --rm -v ./scripts:/scripts ${IMAGE} /scripts/compile_fullaxx_fbpanel.sh
 done
